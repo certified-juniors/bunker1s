6a7f23b9847d6b6551669c0696ac6a2df3021d92
SicParv1sMagna <tayushiev@gmail.com>
2025-07-11 01:03:02 +0300

    Initial commit

---
+++ bunker-backend/node_modules/@swc/cli/lib/swc/index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "swcDir", {
+    enumerable: true,
+    get: function() {
+        return _dir.default;
+    }
+});
+const _dir = /*#__PURE__*/ _interop_require_default(require("./dir"));
+function _interop_require_default(obj) {
+    return obj && obj.__esModule ? obj : {
+        default: obj
+    };
+}
+
+//# sourceMappingURL=index.js.map
---
+++ bunker-backend/src/entities/card/card.enum.ts
@@ -0,0 +1,31 @@
+export enum PROFESSION {
+
+}
+
+export enum HEALTH {
+
+}
+
+export enum HOBBY {
+
+}
+
+export enum PHOBY {
+
+}
+
+export enum HUMAN_QUALITY {
+
+}
+
+export enum ADDITIONAL_INFO {
+
+}
+
+export enum LUGGAGE {
+
+}
+
+export enum SPECIAL_EVENT {
+
+}
